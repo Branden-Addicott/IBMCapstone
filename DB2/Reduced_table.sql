b9f8bff3ac6ee8154b217837ad97dc88336f001a
Branden Addicott <branden.addicott@live.com>
2020-08-16 14:23:33 -0400

    Notebooks

--- DB2/Reduced_table.sql
+++ DB2/Reduced_table.sql
@@ -1,1 +1,1 @@
-create table Coursera.Accident_Reduced(severity int, start_date date, start_day int, start_hour int, Temperature real, visibility real, wind_speed real, Precipitation real)
+create table Coursera.Accident_Reduced(severity int, start_date date, start_year int, start_day int, start_hour int, Temperature real, visibility real, wind_speed real, Precipitation real)

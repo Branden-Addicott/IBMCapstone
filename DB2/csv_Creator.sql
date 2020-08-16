0f4fdac6e4d734e84c4d36f5e5edd419818282f3
Branden Addicott <branden.addicott@live.com>
2020-08-16 15:34:51 -0400

    Notebooks

--- DB2/csv_Creator.sql
+++ DB2/csv_Creator.sql
@@ -1,1 +1,1 @@
-Export to /home/rpe21/Projects/IBMCapstone/CSV/SC_Accident.csv of DEL select * from Coursera.Accident_Reduced
+Export to /home/rpe21/Projects/IBMCapstone/CSV/SC_Accident.csv of DEL Select * From Coursera.Accident_Reduced
